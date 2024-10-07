bf96c78f05aa0b69d2505792a217bda5a01fce3c
Jan Vavřínek <janvavrinek1@gmail.com>
2024-10-07 20:40:16 +0200

    feat: add fonts and colors

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,5 +1,11 @@
 import { type ParentProps, Suspense } from "solid-js";
 
 export default function AuthLayout(props: ParentProps) {
-	return <Suspense>{props.children}</Suspense>;
+	return (
+		<main class="grid h-dvh w-dvw place-content-center overflow-auto bg-gradient-to-b from-pv-blue-300 p-5 dark:from-pv-blue-900 dark:to-pv-blue-950">
+			<div class="rounded-xl border-2 border-pv-blue-900 bg-pv-blue-50 p-5 shadow-lg">
+				<Suspense>{props.children}</Suspense>
+			</div>
+		</main>
+	);
 }
