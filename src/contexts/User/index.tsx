98ea88df46f1cdbb44994b2e66f4fa0068798f99
Jan Vavřínek <janvavrinek1@gmail.com>
2024-12-10 22:25:32 +0100

    feat: add support for realtime communication

--- src/contexts/User/index.tsx
+++ src/contexts/User/index.tsx
@@ -1,6 +1,8 @@
+import supabase from "@lib/supabase";
 import { client } from "@lib/trpc/client";
 import { useLocation, useNavigate } from "@solidjs/router";
-import { type ParentProps, Show, createContext, onMount } from "solid-js";
+import type { RealtimeChannel } from "@supabase/supabase-js";
+import { type Accessor, type ParentProps, Show, createContext, createSignal, onMount } from "solid-js";
 import { type SetStoreFunction, createStore } from "solid-js/store";
 import type { User } from "./types";
 
@@ -12,13 +14,16 @@ const EMPTY_USER: User = {
 export const userContext = createContext<{
 	user: User;
 	setUser: SetStoreFunction<User>;
+	channel: Accessor<RealtimeChannel | undefined>;
 }>({
 	user: EMPTY_USER,
 	setUser: () => {},
+	channel: () => undefined,
 });
 
 export default function UserProvider(props: ParentProps) {
 	const [user, setUser] = createStore<User>({ ...EMPTY_USER });
+	const [channel, setChannel] = createSignal<RealtimeChannel>();
 	const navigate = useNavigate();
 	const location = useLocation();
 
@@ -33,11 +38,20 @@ export default function UserProvider(props: ParentProps) {
 				navigate(`/auth/signin?return_path=${encodeURIComponent(location.pathname)}`);
 			}
 		}
-		if (foundUser) setUser(foundUser);
+		if (foundUser) {
+			setUser(foundUser);
+
+			const c = supabase.channel(foundUser.id, { config: { private: true, broadcast: { self: true } } });
+
+			c.subscribe((status) => {
+				if (status !== "SUBSCRIBED") return;
+				setChannel(c);
+			});
+		}
 	});
 
 	return (
-		<userContext.Provider value={{ user, setUser }}>
+		<userContext.Provider value={{ user, setUser, channel }}>
 			<Show when={user.id.length}>{props.children}</Show>
 		</userContext.Provider>
 	);
